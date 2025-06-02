d70fdf90267a0e52931e300c7a832b48786bb86a
Ryan Stoffel <rstof@Ryans-MacBook-Pro.local>
2025-06-01 19:43:54 -0700

    Fixing Merge Request

---
+++ app/components/CCDC_Section.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import styles from "./teams-section.module.css";
+import React from "react";
+
+const CCDC_Section = () => {
+  return (
+    <>
+			<section id="ccdc" className={`${styles.teamSection} ${styles.active}`}>
+        <h3 className={styles.teamSectionTitle}>CCDC Members</h3>
+        <p className={styles.teamDescription}>
+          The ICPC, or International Collegiate Programming Contest, is a global
+          competitive programming competition...
+        </p>
+        <div className={styles.memberGrid}>
+          <div className={styles.memberCard}>
+            <div className={styles.avatar}>
+              <Image
+                src="/profile.png"
+                alt="CCDC Member"
+                width={250}
+                height={250}
+                className={styles.memberImage}
+              />
+            </div>
+            <p className={styles.name}>Firstname Last</p>
+            <div className={styles.gradEmail}>
+              <p className={styles.gradclass}>Class of 20XX</p>
+              <a
+                href="mailto:test@example.com"
+                className={styles.emailIconContainer}
+              >
+                <Image
+                  src="/EMAIL_ICON.png"
+                  alt="email"
+                  width={16}
+                  height={16}
+                  className={styles.emailIcon}
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+		</>
+  )
+}
+
+export default CCDC_Section
---
+++ app/components/SET_Section.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import styles from "./teams-section.module.css";
+import React from "react";
+
+const SET_Section = () => {
+  return (
+    <>
+			<section id="set" className={`${styles.teamSection} ${styles.active}`}>
+        <h3 className={styles.teamSectionTitle}>SET Members</h3>
+        <p className={styles.teamDescription}>
+          The ICPC, or International Collegiate Programming Contest, is a global
+          competitive programming competition...
+        </p>
+        <div className={styles.memberGrid}>
+          <div className={styles.memberCard}>
+            <div className={styles.avatar}>
+              <Image
+                src="/profile.png"
+                alt="SET Member"
+                width={250}
+                height={250}
+                className={styles.memberImage}
+              />
+            </div>
+            <p className={styles.name}>Firstname Last</p>
+            <div className={styles.gradEmail}>
+              <p className={styles.gradclass}>Class of 20XX</p>
+              <a
+                href="mailto:test@example.com"
+                className={styles.emailIconContainer}
+              >
+                <Image
+                  src="/EMAIL_ICON.png"
+                  alt="email"
+                  width={16}
+                  height={16}
+                  className={styles.emailIcon}
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+		</>
+  )
+}
+
+export default SET_Section
